1944d76123abdb44230c10e50a6e1191ef06161e
Diego Vela <supotuco@gmail.com>
2019-12-30 20:21:49 +0800

    Fix generic type

--- app/src/main/java/com/supotuco/baseproject/dynamicadapter/BaseViewHolder.kt
+++ app/src/main/java/com/supotuco/baseproject/dynamicadapter/BaseViewHolder.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 abstract class BaseViewHolder<T: BaseHolderModel>(view: View): RecyclerView.ViewHolder(view) {
 
-    abstract fun bind(holderModel: BaseHolderModel)
+    abstract fun bind(holderModel: T)
 
     abstract fun  recycle()
 }
